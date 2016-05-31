bad83b4bd597d79b6bdbd997e1ec75819276a907
marius <m.zilenas@litrail.lt>
2016-05-31 09:45:33 +0300

    Yesterday, today, tomorrow support.

---
+++ NaturalLanguageDateTime.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NaturalLanguageDateTime;
+
+namespace NaturalLanguageDateTime.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestToday()
+        {
+            String today = DateTime.Now.Date.ToString();
+            NLDateTime nldt = NLDateTime.ParseString("today");
+            Assert.AreEqual(today, nldt.DateTime.Date.ToString());
+        }
+        [TestMethod]
+        public void TestTomorrow()
+        {
+            String tomorrow = DateTime.Now.AddDays(+1).Date.ToString();
+            NLDateTime nldt = NLDateTime.ParseString("tomorrow");
+            Assert.AreEqual(tomorrow, nldt.Date.ToString());
+        }
+        [TestMethod]
+        public void TestYesterday()
+        {
+            String yesterday = DateTime.Now.AddDays(-1).Date.ToString();
+            NLDateTime nldt = NLDateTime.ParseString("yesterday");
+            Assert.AreEqual(yesterday, nldt.Date.ToString());
+        }
+    }
+}
---
+++ NaturalLanguageDateTime/NLDateTime.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalLanguageDateTime
+{
+    public class NLDateTime
+    {
+        public DateTime DateTime { get; set; }
+        public DateTime Date { get { return DateTime.Date; } }
+
+        public override string ToString()
+        {
+            return DateTime.ToString();
+        }
+             
+        public NLDateTime()
+        { 
+            DateTime = new DateTime();
+        }
+
+        public static NLDateTime ParseString(string str)
+        {
+            return new NLDateTime().Parse(str);
+        }
+
+        public NLDateTime Parse(string str)
+        {
+            foreach (string word in str.Split('.'))
+            { 
+                switch (word.ToLower())
+                {
+                    case "yesterday" :
+                        DateTime = DateTime.Now.AddDays(-1);
+                        break;
+                    case "tomorrow":
+                        DateTime = DateTime.Now.AddDays(+1);
+                        break;
+                    case "today":
+                        DateTime = DateTime.Now; 
+                        break;
+                    case "plus" :
+                        throw new NotImplementedException();
+                    case "minus" :
+                        throw new NotImplementedException();
+                    case "next" :
+                        throw new NotImplementedException();
+                    case "last" :
+                        throw new NotImplementedException();
+                }
+            }
+            return this;
+        }
+    }
+}
